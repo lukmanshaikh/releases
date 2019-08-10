a6b05820275a95a1d0ba4bc487d3f72cfdd29ade
Lukman Shaikh <shaikhlukman01@gmail.com>
2019-08-10 17:05:44 +0000

    Update config.sh
    Signed-off-by: Lukman Shaikh <shaikhlukman01@gmail.com>

--- config.sh
+++ config.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="LukmanShaikh"
+export GITHUB_EMAIL="shaikhlukman01@gmail.com"
 
-export KBUILD_BUILD_USER=""
+export KBUILD_BUILD_USER="Luqman"
 export KBUILD_BUILD_HOST="builder"
 
-export device=""
+export device="land"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export ROM="CrDroid"
+export ROM_DIR="CrDroid"
+export ROM_VERSION="5.6"
+export local_manifest_url="https://github.com/lukmanshaikh/manifests/raw/Crdroid.xml"
+export manifest_url="https://github.com/crdroidandroid/android"
+export rom_vendor_name="lineage"
+export branch="9.0"
 export bacon="bacon"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="LukmanShaikh/Releases"
 
 export timezone="UTC"
